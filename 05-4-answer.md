bc8671d8eac2e1e56456c6ce7b3c994be87cc7a0
Xunkai Zhang <xunkai55@gmail.com>
2016-04-14 19:32:31 +0800

    finish 05-4

---
+++ 05-4-answer.md
@@ -0,0 +1,10 @@
+# 05-4 Answer
+
+2012011486 张洵恺
+
+2012013290 韩珊
+
+## 练习
+
+代码见[lab5-spoc-discuss](05-4-answer/lab5-spoc-discuss)
+
---
+++ 05-4-answer/lab5-spoc-discuss/kern/schedule/sched.c
@@ -0,0 +1,67 @@
+#include <list.h>
+#include <sync.h>
+#include <proc.h>
+#include <sched.h>
+#include <assert.h>
+
+void
+wakeup_proc(struct proc_struct *proc) {
+    assert(proc->state != PROC_ZOMBIE);
+    bool intr_flag;
+    local_intr_save(intr_flag);
+    {
+        if (proc->state != PROC_RUNNABLE) {
+            proc->state = PROC_RUNNABLE;
+            proc->wait_state = 0;
+        }
+        else {
+            warn("wakeup runnable process.\n");
+        }
+    }
+    local_intr_restore(intr_flag);
+}
+
+void
+schedule(void) {
+    bool intr_flag;
+    list_entry_t *le, *last;
+    struct proc_struct *next = NULL;
+    if (current == idleproc) {
+        last = &proc_list;
+    } else {
+        last = &(current->list_link);
+    }
+    do {
+        le = list_next(le);
+        if (le != &proc_list) {
+            next = le2proc(le, list_link);
+            cprintf("schedule pid=%d, name=%s, state=%d\n",
+                next->pid,
+                next->name,
+                next->state);
+        }
+    } while (le != last);
+    local_intr_save(intr_flag);
+    {
+        current->need_resched = 0;
+        last = (current == idleproc) ? &proc_list : &(current->list_link);
+        le = last;
+        do {
+            if ((le = list_next(le)) != &proc_list) {
+                next = le2proc(le, list_link);
+                if (next->state == PROC_RUNNABLE) {
+                    break;
+                }
+            }
+        } while (le != last);
+        if (next == NULL || next->state != PROC_RUNNABLE) {
+            next = idleproc;
+        }
+        next->runs ++;
+        if (next != current) {
+            proc_run(next);
+        }
+    }
+    local_intr_restore(intr_flag);
+}
+
